985357572c306e3638909f14bcaf243d72f00086
david.devops2000 <david.devops2000@gmail.com>
2021-05-05 16:08:29 -0300

    uploading

---
+++ Part 1/q_learning.py
@@ -0,0 +1,4 @@
+import numpy as np #numpy é um lib que tem recursos matemáticos
+
+gamma = 0.75
+alpha = 0.9
---
+++ Part 1/untitled1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  5 15:53:23 2021
+
+@author: devon
+"""
+
