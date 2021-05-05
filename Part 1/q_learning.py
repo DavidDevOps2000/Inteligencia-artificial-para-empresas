5ba4dfc0ecd15f0b2993197de57a44cc84400541
david.devops2000 <david.devops2000@gmail.com>
2021-05-05 16:19:57 -0300

    adicionando dicionário e listas

--- Part 1/q_learning.py
+++ Part 1/q_learning.py
@@ -2,3 +2,18 @@ import numpy as np #numpy é um lib que tem recursos matemáticos
 
 gamma = 0.75
 alpha = 0.9
+
+location_to_state = {'A':0,
+                     'B':1,
+                     'C':2,
+                     'D':3,
+                     'E':4,
+                     'F':5,
+                     'G':6,
+                     'H':7,
+                     'I':8,
+                     'J':9,
+                     'K':10,
+                     'L':11}
+  
+action = [0,1,2,3,4,5,6,7,8,9,10,11]
